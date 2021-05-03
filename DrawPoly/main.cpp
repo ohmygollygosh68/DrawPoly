b6cb2321c9c54424ba23bd2ae9f3f38a97d67ea2
Steven Godbold <steven_godbold@hotmail.com>
2021-05-03 13:41:01 +0100

    GLEW test code

--- DrawPoly/main.cpp
+++ DrawPoly/main.cpp
@@ -1,109 +1,33 @@
-#include <GL/glew.h>  
-#include <GLFW/glfw3.h> 
-#include <iostream>
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 
-// Test code for glew and glfw
-
-void window_resized(GLFWwindow* window, int width, int height);
-void key_pressed(GLFWwindow* window, int key, int scancode, int action, int mods);
-void show_glfw_error(int error, const char* description);
-
-
-int main()
-{
-    glfwSetErrorCallback(show_glfw_error);
-
-
-    if (!glfwInit()) {
-        std::cerr << "GLFW" << '\n';
-        exit(-1);
-    }
-
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-
-    GLFWwindow* window = glfwCreateWindow(
-        800, // width
-        600, // height
-        "OpenGL Example",
-        NULL, NULL);
-    if (!window)
-    {
-        std::cerr << "not window error" << '\n';
-        glfwTerminate();
-        exit(-1);
-    }
-
-
-    glfwMakeContextCurrent(window);
-
-
-    glfwSetWindowSizeCallback(window, window_resized);
-    glfwSetKeyCallback(window, key_pressed);
-
-
-    glfwSwapInterval(1);
-
-
-    glewExperimental = GL_TRUE;
-    GLenum err = glewInit();
-    if (err != GLEW_OK) {
-        std::cerr << "GLEW error " << glewGetErrorString(err) << '\n';
-        glfwTerminate();
-        exit(-1);
-    }
-
-
-    std::cout << glGetString(GL_VERSION) << '\n';
-
-
-    int nr_extensions = 0;
-    glGetIntegerv(GL_NUM_EXTENSIONS, &nr_extensions);
-
-    for (int i = 0; i < nr_extensions; ++i) {
-        std::cout << glGetStringi(GL_EXTENSIONS, i) << '\n';
-    }
-
-
-    glClearColor(0, 0, 1, 1);
-
-
-    while (!glfwWindowShouldClose(window)) {
-
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-
-        glfwPollEvents();
-    }
-
-
-    glfwDestroyWindow(window);
-
-
-    glfwTerminate();
-    return 0;
+void MyDisplay() {                      //
+	glClear(GL_COLOR_BUFFER_BIT);       //GL
+	glViewport(0, 0, 300, 300);
+	glColor3f(1.0, 1.0, 1.0);
+	glBegin(GL_POLYGON);                //
+	glVertex3f(-0.5, -0.5, 0.0);
+	glVertex3f(0.5, -0.5, 0.0);
+	glVertex3f(0.5, 0.5, 0.0);
+	glVertex3f(-0.5, 0.5, 0.0);
+	glEnd();
+	glFlush();
 }
 
-void show_glfw_error(int error, const char* description) {
-    std::cerr << "Error: " << description << '\n';
-}
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);               //GLUT
+	glutInitDisplayMode(GLUT_RGB);
+	glutInitWindowSize(300, 300);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("OpenGL Sample Drawing");
 
-void window_resized(GLFWwindow* window, int width, int height) {
-    std::cout << "Window resized, new window size: " << width << " x " << height << '\n';
+	glClearColor(0.0, 0.0, 0.0, 1.0);   //GL
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
 
-    glClearColor(0, 0, 1, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
-}
-
-void key_pressed(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == 'Q' && action == GLFW_PRESS) {
-        glfwTerminate();
-        exit(0);
-    }
+	glutDisplayFunc(MyDisplay);         //GLUT
+	glutMainLoop();                     //
+	return 0;
 }
