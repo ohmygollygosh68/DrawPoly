0731f8db79f770eedc857dbb893acb28b1b5a29c
Steven Godbold <steven_godbold@hotmail.com>
2021-05-03 16:49:36 +0100

    Draw a triangle and a quad

--- DrawPoly/main.cpp
+++ DrawPoly/main.cpp
@@ -1,33 +1,65 @@
+#include<windows.h>
+#include <iostream>
+#include <stdlib.h>
 #include <GL/glut.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
-
-void MyDisplay() {                      //
-	glClear(GL_COLOR_BUFFER_BIT);       //GL
-	glViewport(0, 0, 300, 300);
-	glColor3f(1.0, 1.0, 1.0);
-	glBegin(GL_POLYGON);                //
-	glVertex3f(-0.5, -0.5, 0.0);
-	glVertex3f(0.5, -0.5, 0.0);
-	glVertex3f(0.5, 0.5, 0.0);
-	glVertex3f(-0.5, 0.5, 0.0);
-	glEnd();
-	glFlush();
+using namespace std;
+//Called when a key is pressed
+void handleKeypress(unsigned char key, int x, int y) {
+	switch (key) {
+	case 27: //Escape key
+		exit(0);
+	}
 }
-
+//Initializes 3D rendering
+void initRendering() {
+	//Makes 3D drawing work when something is in front of something else
+	glEnable(GL_DEPTH_TEST);
+}
+//Called when the window is resized
+void handleResize(int w, int h) {
+	//Tell OpenGL how to convert from coordinates to pixel values
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION); //Switch to setting the camera perspective
+	//Set the camera perspective
+	glLoadIdentity(); //Reset the camera
+	gluPerspective(45.0,                  //The camera angle
+		(double)w / (double)h, //The width-to-height ratio
+		1.0,                   //The near z clipping coordinate
+		200.0);                //The far z clipping coordinate
+}
+//Draws the 3D scene
+void drawScene() {
+	//Clear information from last draw
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
+	glLoadIdentity(); //Reset the drawing perspective
+	glBegin(GL_QUADS); //Begin quadrilateral coordinates
+	//Trapezoid
+	glVertex3f(-0.7f, -1.5f, -5.0f);
+	glVertex3f(0.7f, -1.5f, -5.0f);
+	glVertex3f(0.7f, -0.5f, -5.0f);
+	glVertex3f(-0.7f, -0.5f, -5.0f);
+	glEnd(); //End quadrilateral coordinates
+	glBegin(GL_TRIANGLES); //Begin triangle coordinates
+	//Triangle
+	glVertex3f(-0.5f, 0.5f, -5.0f);
+	glVertex3f(-1.0f, 1.5f, -5.0f);
+	glVertex3f(-1.5f, 0.5f, -5.0f);
+	glEnd(); //End triangle coordinates
+	glutSwapBuffers(); //Send the 3D scene to the screen
+}
 int main(int argc, char** argv) {
-	glutInit(&argc, argv);               //GLUT
-	glutInitDisplayMode(GLUT_RGB);
-	glutInitWindowSize(300, 300);
-	glutInitWindowPosition(0, 0);
-	glutCreateWindow("OpenGL Sample Drawing");
-
-	glClearColor(0.0, 0.0, 0.0, 1.0);   //GL
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-
-	glutDisplayFunc(MyDisplay);         //GLUT
-	glutMainLoop();                     //
+	//Initialize GLUT
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(400, 400); //Set the window size
+	//Create the window
+	glutCreateWindow("Basic Shapes - programming-technique.blogspot.com");
+	initRendering(); //Initialize rendering
+	//Set handler functions for drawing, keypresses, and window resizes
+	glutDisplayFunc(drawScene);
+	glutKeyboardFunc(handleKeypress);
+	glutReshapeFunc(handleResize);
+	glutMainLoop(); //Start the main loop
 	return 0;
 }
